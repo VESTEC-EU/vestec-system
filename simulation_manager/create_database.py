a96752e1bcde53a8fbf36739cbdb2ee382df9a44
Gordon Gibb <gordon.gibb@ed.ac.uk>
2019-04-04 17:31:43 +0100

    Removed the old simulation_manager directory

--- simulation_manager/create_database.py
+++
@@ -1,5 +0,0 @@
-import database
-import os
-
-os.system("rm database/vestec.sqlite")
-database.generate_db.generate()
--- simulation_manager/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import db, initialiseDatabase
-from . import generate_db
-from . import job
-from . import machine
