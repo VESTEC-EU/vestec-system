340b0e594b2c2291bf7b794db5c942badd52c5b7
Rupert Nash <r.nash@epcc.ed.ac.uk>
2019-04-29 16:42:39 +0100

    basic lint of connection manager package

--- ConnectionManager/machines.py
+++ ConnectionManager/machines.py
@@ -2,15 +2,15 @@
 machines ={}
 
 machines["ARCHER"]  = {
-"host": "login.archer.ac.uk",
-"username": "gpsgibb",
-"SSHkey": "rsa_archer",
-"basedir": "/work/z01/z01/gpsgibb/vestec"
-}
+    "host": "login.archer.ac.uk",
+    "username": "gpsgibb",
+    "SSHkey": "rsa_archer",
+    "basedir": "/work/z01/z01/gpsgibb/vestec"
+    }
 
 machines["CIRRUS"] = {
-"host": "cirrus.epcc.ed.ac.uk",
-"username": "gpsgibb",
-"SSHkey": "rsa_cirrus",
-"basedir": "/lustre/home/z04/gpsgibb/vestec"
-}
+    "host": "cirrus.epcc.ed.ac.uk",
+    "username": "gpsgibb",
+    "SSHkey": "rsa_cirrus",
+    "basedir": "/lustre/home/z04/gpsgibb/vestec"
+    }
