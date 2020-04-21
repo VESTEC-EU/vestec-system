f7e1551bad97eade51095fc3122cc506e0eef892
Max Kontak <Max.Kontak@DLR.de>
2020-04-21 14:25:36 +0200

    DataManager: database entities for data transfers

--- DataManager/db.py
+++ DataManager/db.py
@@ -17,6 +17,18 @@ class Data(db.Entity):
     status = pny.Required(str,default="ACTIVE") #could be ACTIVE, ARCHIVED, DELETED or UNKNOWN
     originator = pny.Required(str) #where the data came from, e.g. a website, simulation output, some user
     group = pny.Required(str) #what the data belongs to, e.g. fire, space weather etc
+    
+class DataTranfers(db.Entity):
+    """ Database entity for data transfers """
+    id = pny.PrimaryKey(str)
+    src_id = pny.Required(str) # allows to get file size from database
+    dst_id = pny.Required(str) # might be different from src_id on copy
+    src_machine = pny.Required(str)
+    dst_machine = pny.Required(str)
+    #date_submitted = pny.Required(datetime.datetime) # might be necessary for non-blocking data transfer
+    date_started = pny.Required(datetime.datetime)
+    date_completed = pny.Optional(datetime.datetime)
+    completion_time = pny.Optional(datetime.timedelta)
 
 
 def initialise_database():
