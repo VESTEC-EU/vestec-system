6221a9f6bc52265c400cf10a978bda029de2eca7
Nick Brown <nick@mesham.com>
2020-06-10 20:25:45 +0100

    Reorganisation of machine and queue persistence objects

--- Database/machine.py
+++ Database/machine.py
@@ -1,6 +1,7 @@
 from Database import db
 import pony.orm as pny
 from enum import Enum
+import datetime
 
 
 class Machine(db.Entity):
@@ -11,5 +12,7 @@ class Machine(db.Entity):
     num_nodes = pny.Required(int)
     cores_per_node = pny.Required(int)
     base_work_dir = pny.Required(str)
-
-    queues = pny.Set("Queue")
+    enabled = pny.Required(bool, default=False, sql_default='0')
+    
+    status = pny.Optional(str)
+    status_last_checked = pny.Optional(datetime.datetime)
