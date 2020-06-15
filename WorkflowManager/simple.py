cf82ae86801e6cfb5bb4fe47c4fb2276f30804f5
Nick Brown <nick@mesham.com>
2020-06-15 22:05:59 +0100

    Simple workflow example of driving new simulation and queue state call completion

--- WorkflowManager/simple.py
+++ WorkflowManager/simple.py
@@ -13,10 +13,12 @@ from Database.workflow import Incident, StoredDataset
 
 if "VESTEC_EDI_URI" in os.environ:    
     EDI_URL= os.environ["VESTEC_EDI_URI"]
+    SM_URL= os.environ["VESTEC_SM_URI"]
     DATA_MANAGER_URI = os.environ["VESTEC_DM_URI"]
 else:    
     EDI_URL= 'http://localhost:5501/EDImanager'
     DATA_MANAGER_URI = 'http://localhost:5000/DM'
+    SM_URL = 'http://localhost:5505/SM'
 
 # we now want to define some handlers
 @workflow.handler
@@ -49,6 +51,14 @@ def manually_add_data(message):
 @workflow.handler
 def test_workflow(message):
     print("Test called!")
+    callbacks = {'COMPLETED': 'simple_workflow_execution_completed'}
+    myobj = {'incident_id': message["IncidentID"], 'num_nodes': 100, 'requested_walltime': 120, 'executable': 'mytest.exe', 'queuestate_calls':callbacks}
+    x = requests.post(SM_URL+'/create', json=myobj)
+    print(x.json())
+
+@workflow.handler
+def simple_workflow_execution_completed(message):
+    print("Stage completed with simulation ID "+message["simulationId"])
 
 @workflow.handler
 def initialise_simple(message):
@@ -72,3 +82,4 @@ def RegisterHandlers():
     workflow.RegisterHandler(manually_add_data, "add_data_simple")
     workflow.RegisterHandler(test_workflow, "test_workflow_simple")
     workflow.RegisterHandler(initialise_simple, "initialise_simple")
+    workflow.RegisterHandler(simple_workflow_execution_completed, "simple_workflow_execution_completed")
