520173b50ec298b833f247ca84d9584f2d5751ce
Nick Brown <nick@mesham.com>
2020-06-17 15:26:21 +0100

    Machine interface injects in machine connection and scheduler parser based on DB contents

--- MachineInterface/mproxy/server/machine.py
+++ MachineInterface/mproxy/server/machine.py
@@ -2,13 +2,22 @@ import sys
 sys.path.append("../")
 from Database.machine import Machine
 import pony.orm as pny
-from .pbs_queue import PBSQueueProcessor
 import fabric
 
 class MachineConnectionFactory:
     def __init__(self, configs):        
         self.configs = configs
         self._machine_cache = {}
+        self.machine_connections = { "fabricssh": self._mk_fab_machine_connection,
+                                     "openssh": self._mk_openssh_machine_connection,
+                                     "dummy": self._mk_dummy_machine_connection }
+
+        self.queue_processors = { "pbs" : self._mk_pbs_queue_processor }
+
+    def _mk_pbs_queue_processor(self):
+        from .pbs_queue import PBSQueueProcessor
+
+        return PBSQueueProcessor()
 
     def _mk_fab_conn(self, conf):
         host = conf["hostname"]
@@ -17,22 +26,20 @@ class MachineConnectionFactory:
 
         return fabric.Connection(
             host, user=user, connect_kwargs={"key_filename": keyfile}
-        )
+        )    
 
-    def _mk_fab_machine_connection(self, name, queue_system, conf):
+    def _mk_fab_machine_connection(self, name, queue_system, machine, conf):
         from .fabric_machine import FabricMachineConnection
 
-        fabconn = self._mk_fab_conn(conf)
-        d = conf["remote_base_dir"]
-        return FabricMachineConnection(queue_system, fabconn, d)
+        fabconn = self._mk_fab_conn(conf)        
+        return FabricMachineConnection(queue_system, fabconn, machine.base_work_dir)
 
-    def _mk_openssh_machine_connection(self, name, queue_system, conf):
+    def _mk_openssh_machine_connection(self, name, queue_system, machine, conf):
         from .openssh_machine import OpenSSHMachineConnection
+        
+        return OpenSSHMachineConnection(queue_system, machine.host_name, machine.base_work_dir)
 
-        d = conf["remote_base_dir"]
-        return OpenSSHMachineConnection(queue_system, conf["hostname"], conf["remote_base_dir"])
-
-    def _mk_dummy_machine_connection(self, name, queue_system, conf):
+    def _mk_dummy_machine_connection(self, name, queue_system, machine, conf):
         from .dummy_machine import DummyMachineConnection
 
         return DummyMachineConnection(name)
@@ -59,13 +66,13 @@ class MachineConnectionFactory:
                 else:
                     conf = self.getConfiguration(name)
                     # Choose a factory function based on type
-                    ff = {
-                        "fabricssh": self._mk_fab_machine_connection,
-                        "openssh": self._mk_openssh_machine_connection,
-                        "dummy": self._mk_dummy_machine_connection,
-                    }[conf["type"]]
-                    machine = ff(name, PBSQueueProcessor(), conf) #need queue system for this and dummy!
-                    self._machine_cache[name] = machine
-                    return machine
+                    if stored_machine.connection_type in self.machine_connections and stored_machine.scheduler in self.queue_processors:
+                        selectedMachineMechanism=self.machine_connections[stored_machine.connection_type]
+                        selectedQueueProcessor=self.queue_processors[stored_machine.scheduler]
+                        createdMachineMechanism = selectedMachineMechanism(name, selectedQueueProcessor(), stored_machine, conf) 
+                        self._machine_cache[name] = createdMachineMechanism
+                        return createdMachineMechanism
+                    else:
+                        print("Error, either machine connection type or scheduler is not recognised")
 
     pass
