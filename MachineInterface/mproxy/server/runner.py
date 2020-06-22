c334d854769ca2f325426630c8ad0aaa776164e6
Nick Brown <nick@mesham.com>
2020-06-22 12:23:20 +0100

    Connection to system RabbitMQ server from machine interface

--- MachineInterface/mproxy/server/runner.py
+++ MachineInterface/mproxy/server/runner.py
@@ -3,7 +3,8 @@ import aio_pika
 from .machine import MachineConnectionFactory
 from .rpc_server import RpcServer
 from ..core.connect import aio_pika_connect_params
-
+import os
+import time
 
 class ServerRunner:
     def __init__(self, config, names):
@@ -16,9 +17,25 @@ class ServerRunner:
         """Create a connection and start all the servers listening to
         it.
         """
-        self.connection = await aio_pika.connect(
-            loop=loop, host="localhost"
-        )
+        if "VESTEC_RMQ_SERVER" in os.environ:
+            host = os.environ["VESTEC_RMQ_SERVER"]            
+        else:
+            print("Environment variable VESTEC_RMQ_SERVER not set. Defaulting to `localhost`")
+            host="localhost"
+
+        for i in range(5):
+            print(" [*] Opening connection to RabbitMQ server")
+            try:
+                self.connection = await aio_pika.connect(loop=loop, host=host)
+            except:
+                if i<4:
+                    print("Cannot connect to RabbitMQ server. Will try again in 5 seconds...")
+                    time.sleep(5)
+                else:
+                    print("Cannot create connection to AMQP server... Maybe it's down?")
+                    raise
+            else:
+                break
 
         self.servers = {
             name: RpcServer(name, self.mc_factory, self.connection)
