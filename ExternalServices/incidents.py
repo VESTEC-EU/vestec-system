385cb0de290fa98ca064b5b833fe3377a83daeb0
Max Kontak <Max.Kontak@DLR.de>
2020-07-09 09:41:02 +0200

    workflow_graph: filter out Nones in completion times

--- ExternalServices/incidents.py
+++ ExternalServices/incidents.py
@@ -41,24 +41,24 @@ def generateIncidentDiGraph(incident_uuid):
         for m in messages:
             originator = m.originator
             destination = m.destination
-            
+
             #remove handler or queue suffixes from names if present (just for cosmetic reasons)
             originator= originator.replace("_handler","")
             destination = destination.replace("_queue","")
-            
+
             #if the src and dest tags are present, use these as node names instead of the originator and destination fields
             if m.src_tag != "":
                 originator = m.src_tag
             if m.dest_tag != "":
                 destination = m.dest_tag
-            
+
             #don't display any handlers internal to the workflow system
             if originator[0] == "_":
                 continue
-            
+
             #create nodes and join them together
             G.add_node(originator,style="filled",fillcolor="chartreuse") #originator clearly called successfully
-            
+
             if m.status == "SENT":
                 colour="white"
             elif m.status == "COMPLETE":
@@ -75,21 +75,22 @@ def generateIncidentDiGraph(incident_uuid):
             elif m.date_completed is None:
                 tooltip_msg+=", message received and stage started on "+m.date_received.strftime("%d/%m/%Y, %H:%M:%S")
             else:
-                tooltip_msg+=", stage completed on "+m.date_completed.strftime("%d/%m/%Y, %H:%M:%S")            
+                tooltip_msg+=", stage completed on "+m.date_completed.strftime("%d/%m/%Y, %H:%M:%S")
 
             G.add_node(destination,style="filled",fillcolor=colour, tooltip=tooltip_msg)
             G.add_edge(originator,destination)
 
         for node in G:
-            #completion_time = sum(
-            #    (
-            #        m.completion_time
-            #        for m in messages
-            #        if node in [m.destination, m.dest_tag]
-            #    ),
-            #    datetime.timedelta(0),
-            #)
-            G.nodes[node]["label"] = node #+ " " + "(" + str(completion_time) + ")"
+            completion_time = sum(
+                filter(
+                    None,
+                    (m.completion_time
+                    for m in messages
+                    if node in [m.destination, m.dest_tag])
+                ),
+                datetime.timedelta(0),
+            )
+            G.nodes[node]["label"] = node + " " + "(" + str(completion_time) + ")"
 
     return to_agraph(G)
 
@@ -97,14 +98,14 @@ def packageSimulation(sim):
     simulation_dict={}
     simulation_dict["uuid"]=sim.uuid
     if sim.jobID is not None and sim.jobID != "":
-        simulation_dict["jobID"]=sim.jobID        
+        simulation_dict["jobID"]=sim.jobID
 
     simulation_dict["status"]=sim.status
     simulation_dict["status_updated"]=sim.status_updated.strftime("%d/%m/%Y, %H:%M:%S")
 
     if sim.status_message is not None and sim.status_message != "":
         simulation_dict["status_message"]=sim.status_message
-                
+
     simulation_dict["created"]=sim.date_created.strftime("%d/%m/%Y, %H:%M:%S")
     simulation_dict["walltime"]=sim.walltime
     simulation_dict["kind"]=sim.kind
@@ -122,7 +123,7 @@ def packageDataset(stored_ds):
     stored_ds_dict["comment"]=stored_ds.comment
     stored_ds_dict["date_created"]=stored_ds.date_created.strftime("%d/%m/%Y, %H:%M:%S")
     try:
-        data_info=getInfoForDataInDM(stored_ds.uuid)                
+        data_info=getInfoForDataInDM(stored_ds.uuid)
         stored_ds_dict["machine"]=data_info["machine"]
     except DataManagerException as err:
         print("Can not retrive data info from DM "+err.message)
@@ -162,19 +163,19 @@ def packageIncident(stored_incident, include_sort_key, include_digraph, include_
     incident["kind"]=stored_incident.kind
     incident["name"]=stored_incident.name
     incident["status"]=stored_incident.status
-    incident["comment"]=stored_incident.comment         
+    incident["comment"]=stored_incident.comment
     incident["creator"]=stored_incident.user_id.username
     if stored_incident.duration is not None:
         incident["duration"]=stored_incident.duration
-    incident["date_started"]=stored_incident.date_started.strftime("%d/%m/%Y, %H:%M:%S")    
-    
+    incident["date_started"]=stored_incident.date_started.strftime("%d/%m/%Y, %H:%M:%S")
+
     incident_workflow=RegisteredWorkflow.get(kind=stored_incident.kind)
     if incident_workflow is not None:
         incident["test_workflow"]=incident_workflow.test_workflow
 
     if include_associated_simulations:
         incident["simulations"]=[]
-        for sim in stored_incident.simulations:                                   
+        for sim in stored_incident.simulations:
             incident["simulations"].append(packageSimulation(sim))
 
         incident["simulations"]=sorted(incident["simulations"], key=itemgetter('created'), reverse=True)
@@ -189,7 +190,7 @@ def packageIncident(stored_incident, include_sort_key, include_digraph, include_
     if (include_sort_key): incident["srt_key"]=stored_incident.incident_date
     if (include_digraph):
         incident["digraph"]=str(generateIncidentDiGraph(stored_incident.uuid))
-    if (include_manual_data_queuename):        
+    if (include_manual_data_queuename):
         incident["data_queue_name"]=incident_workflow.data_queue_name
     if (include_associated_data):
         incident["data_sets"]=[]
@@ -239,7 +240,7 @@ def retrieveDataMetaData(data_uuid, incident_uuid, username):
     user = User.get(username=username)
     if checkIfUserCanAccessIncident(incident, user):
         for stored_ds in incident.associated_datasets:
-            if stored_ds.uuid==data_uuid: 
+            if stored_ds.uuid==data_uuid:
                 stored_ds_dict={}
                 stored_ds_dict["uuid"]=stored_ds.uuid
                 stored_ds_dict["name"]=stored_ds.name
@@ -255,7 +256,7 @@ def removeDataFromIncident(data_uuid, incident_uuid, username):
     user = User.get(username=username)
     if checkIfUserCanAccessIncident(incident, user):
         for stored_ds in incident.associated_datasets:
-            if stored_ds.uuid==data_uuid: 
+            if stored_ds.uuid==data_uuid:
                 stored_ds.delete()
                 return True
     return False
@@ -275,12 +276,12 @@ def archiveIncident(incident_uuid, username):
     try:
         incident = Incident[incident_uuid]
         user = User.get(username=username)
-        if checkIfUserCanAccessIncident(incident, user): 
+        if checkIfUserCanAccessIncident(incident, user):
             incident.status="ARCHIVED"
             return True
         else:
             return False
-    except pny.core.ObjectNotFound as e:        
+    except pny.core.ObjectNotFound as e:
         return False
 
 @pny.db_session
@@ -288,7 +289,7 @@ def activateIncident(incident_uuid, username):
     try:
         incident = Incident[incident_uuid]
         user = User.get(username=username)
-        if checkIfUserCanAccessIncident(incident, user): 
+        if checkIfUserCanAccessIncident(incident, user):
             incident_workflow=RegisteredWorkflow.get(kind=incident.kind)
             if incident_workflow == None:
                 print("Warning: 'incident_workflow' not defined")
@@ -311,22 +312,22 @@ def activateIncident(incident_uuid, username):
 
 def doesStoredIncidentMatchFilter(stored_incident, pending_filter, active_filter, completed_filter, cancelled_filter, error_filter, archived_filter):
     if stored_incident.status=="PENDING":
-        if pending_filter: return True        
+        if pending_filter: return True
         return False
     if stored_incident.status=="ACTIVE":
-        if active_filter: return True        
+        if active_filter: return True
         return False
     if stored_incident.status=="COMPLETE":
-        if completed_filter: return True        
+        if completed_filter: return True
         return False
     if stored_incident.status=="CANCELLED":
-        if cancelled_filter: return True        
+        if cancelled_filter: return True
         return False
     if stored_incident.status=="ERROR":
-        if error_filter: return True        
+        if error_filter: return True
         return False
     if stored_incident.status=="ARCHIVED":
-        if archived_filter: return True        
+        if archived_filter: return True
         return False
     return False
 
@@ -343,7 +344,7 @@ def retrieveMyIncidentSummary(username, pending_filter, active_filter, completed
     return sorted_incidents
 
 @pny.db_session
-def retrieveIncident(incident_uuid, username):    
+def retrieveIncident(incident_uuid, username):
     user = User.get(username=username)
     incident = Incident.get(uuid=incident_uuid)
     if checkIfUserCanAccessIncident(incident, user):
