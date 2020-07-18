764e498e4d962f7ca1efc9443ee9170ac44fde9e
Nick Brown <nick@mesham.com>
2020-07-18 13:30:01 +0100

    Improved configuration of WFA

--- WorkflowManager/wildfire/wildfire.py
+++ WorkflowManager/wildfire/wildfire.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append("../")
 import pony.orm as pny
-from Database.workflow import Simulation
+from Database.workflow import Simulation, Incident
 from SimulationManager.client import createSimulation, submitSimulation, SimulationManagerException, cancelSimulation
-from DataManager.client import moveDataViaDM, DataManagerException, getInfoForDataInDM, putByteDataViaDM
+from DataManager.client import moveDataViaDM, DataManagerException, getInfoForDataInDM, putByteDataViaDM, registerDataWithDM
 import workflow
 import yaml
 
@@ -54,14 +54,10 @@ def wildfire_fire_simulation(msg):
         print("Dependencies met for WFA")
 
         try:
-            data_info=getInfoForDataInDM(weatherDataUUID)            
+            weather_data_info=getInfoForDataInDM(weatherDataUUID)            
         except DataManagerException as err:
             print("Can not retrieve DM information for weather data "+err.message)
             return
-
-        sample_configuration_file = open("wildfire/templates/wfa.yml")
-        yaml_template = yaml.load(sample_configuration_file, Loader=yaml.FullLoader)
-        yaml_template["weather_data"]["path"]=data_info["absolute_path"]
 
         try:
             callbacks = {'COMPLETED': 'wildfire_fire_results'}
@@ -72,10 +68,10 @@ def wildfire_fire_simulation(msg):
                 machine_basedir=simulation.machine.base_work_dir
                 if machine_basedir[-1] != "/": machine_basedir+="/"
 
-            moveDataViaDM(hotspotDataUUID, machine_basedir+simulation.directory+"/CONFIG_PROB_DYN.json", machine_name)
+            moveDataViaDM(hotspotDataUUID, machine_basedir+simulation.directory+"/WFA_hotspots.json", machine_name)
 
             try:                
-                putByteDataViaDM("wfa.yml", machine_name, "Wildfire configuration", "text/plain", "Wildfire workflow", yaml.dump(yaml_template), path=simulation.directory)                 
+                putByteDataViaDM("wfa.yml", machine_name, "Wildfire configuration", "text/plain", "Wildfire workflow", _buildWFAYaml(IncidentID, weather_data_info), path=simulation.directory)                 
             except DataManagerException as err:
                 print("Can not write wildfire configuration to simulation location"+err.message)
                 return
@@ -84,10 +80,57 @@ def wildfire_fire_simulation(msg):
         except SimulationManagerException as err:
             print("Error creating or submitting WFA simulation "+err.message)
             return
+
+@pny.db_session
+def _buildWFAYaml(incidentId, weather_datainfo):
+    myincident = Incident[incidentId]
+    upperLeft = myincident.upper_left_latlong
+    lowerRight = myincident.lower_right_latlong
+    duration = myincident.duration
+            
+    upperLeftLon, upperLeftLat = upperLeft.split("/")
+    lowerRightLon, lowerRightLat = lowerRight.split("/")
+
+    sample_configuration_file = open("wildfire/templates/wfa.yml")
+    yaml_template = yaml.load(sample_configuration_file, Loader=yaml.FullLoader)    
+    yaml_template["upperleft"]["lat"]=float(upperLeftLat)
+    yaml_template["upperleft"]["lon"]=float(upperLeftLon)
+    yaml_template["lowerright"]["lat"]=float(lowerRightLat)
+    yaml_template["lowerright"]["lon"]=float(lowerRightLon)
+
+    yaml_template["weather_data"]["path"]=weather_datainfo["absolute_path"]
+    yaml_template["dynamic_config"]["path"]="WFA_hotspots.json"
+    yaml_template["sim_duration"]=duration
+        
+    return yaml.dump(yaml_template)
         
 @workflow.handler
 def wildfire_fire_results(msg):
-    print("\nResults available for wildfire analyst simulation!")    
+    IncidentID = msg["IncidentID"]
+    simulationId = msg["simulationId"]
+    print("\nResults available for wildfire analyst simulation!") 
+
+    with pny.db_session:
+        simulation=Simulation[simulationId]
+        machine_name=simulation.machine.machine_name
+
+    if simulation is not None:
+        try:                
+            data_uuid_test_fire_best=registerDataWithDM("test_Fire_Best.tif", machine_name, "WFA simulation", "application/octet-stream", 0, "Normal GTIF", 
+                path=simulation.directory, associate_with_incident=True, incidentId=IncidentID, kind="WFA output file", 
+                comment="Created by WFA on "+machine_name)
+            data_uuid_test_fireshed_best=registerDataWithDM("test_FireShed_Best.tif", machine_name, "WFA simulation", "application/octet-stream", 0, "FireShed GTIF", 
+                path=simulation.directory, associate_with_incident=True, incidentId=IncidentID, kind="WFA output file", 
+                comment="Created by WFA on "+machine_name)
+            data_uuid_test_variance=registerDataWithDM("test_Fire_Variance.tif", machine_name, "WFA simulation", "application/octet-stream", 0, "Variance GTIF", 
+                path=simulation.directory, associate_with_incident=True, incidentId=IncidentID, kind="WFA output file", 
+                comment="Created by WFA on "+machine_name)
+            data_uuid_test_mean=registerDataWithDM("test_Fire_Mean.tif", machine_name, "WFA simulation", "application/octet-stream", 0, "Mean GTIF", 
+                path=simulation.directory, associate_with_incident=True, incidentId=IncidentID, kind="WFA output file", 
+                comment="Created by WFA on "+machine_name)
+        except DataManagerException as err:
+            print("Error registering WFA result data with data manager "+err.message)
+            return   
 
 def RegisterHandlers():
     workflow.RegisterHandler(wildfire_fire_simulation,"wildfire_fire_simulation")
