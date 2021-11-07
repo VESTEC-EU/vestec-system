bdc76f24a5eb8c5c5d0b1ffb3028d47dd6a74b9d
Nick Brown <nick@mesham.com>
2021-11-07 12:00:00 +0000

    Updated git number extraction script based on main branch

--- ExternalServices/misc/gitnumber.sh
+++ ExternalServices/misc/gitnumber.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pushd ../.. > /dev/null
-version_id=`git rev-list --branches master | wc -l`
+version_id=`git rev-list --branches main | wc -l`
 popd > /dev/null
 echo "VERSION_POSTFIX=\"$version_id\"" > "../version_info.py"
